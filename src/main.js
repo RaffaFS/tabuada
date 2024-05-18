c9ad4654a8a1a5d017f0606ae8e01da3342dfd5a
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2024-05-18 11:06:56 -0300

    atualizando a const times linha 9 main.js a fim de deixá-la otimizada para o uso de escalas maiores

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ function calcular(e){
     e.preventDefault();
     const uValue = parseInt(document.querySelector('#user_value').value)
     const resBox = document.querySelector('#result_box')
-    const times = [1,2,3,4,5,6,7,8,9,10]
+    const times = [...Array(10).keys()].map(i => i+1)
     resBox.innerHTML = ''
 
     times.forEach(function(n){
